eafe45d2354267b5eaa96a83637fc660cc702b0c
atharvaupare <atharvaupare5@gmail.com>
2023-10-14 22:36:45 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-3xl text-blue-600'>
+        Hello World
+      </div>
+      <div>hi</div>
+    </>
+  )
+}
+
+export default App
